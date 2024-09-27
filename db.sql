d3caba2bfaba5466f21c5d73adbb9f73220cac2c
ReactedTitan880 <thomasnardou@icloud.com>
2024-09-27 09:23:33 +0200

    [BACKEND] feat: add Sql script for database

---
+++ db.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS db_serverless;
+
+CREATE DATABASE db_serverless;
+
+CREATE TABLE t_users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE t_messages (
+    message_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT, 
+    message_text TEXT NOT NULL,
+    sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES t_users(user_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
